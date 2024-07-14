68eaa1bb60195ef9ffb62dc256eb341a2dac9ae2
ducminh2k2 <duckl2002vn@gmail.com>
2024-07-14 16:03:58 +0700

    second

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>; // Lenh nay yeu cau 1 ket qua kieu CreateProductResult
+    public record CreateProductResult(Guid Id);
+
+    //lớp triển khai IRequestHandler. IRequestHandler<CreateProductCommand, CreateProductResult> Xu ly lenh CreateProductCommand và trả về CreateProductResult
+    internal class CreateProductCommandHandler (IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        //Phương thức Handle nhận lệnh CreateProductCommand và một CancellationToken.
+
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create product emtity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //TODO
+            //save database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //return CreateProductResult result;
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
